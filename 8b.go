d0ccc2a0a3c7403475b26611925bb3e9f99ed150
henry-lang <henry.langmack@gmail.com>
2021-12-08 19:36:12 -0500

    part 2

--- 8b.go
+++ 8b.go
@@ -6,9 +6,24 @@ import (
 	"log"
 	"os"
 	"strings"
+	"strconv"
+	"sort"
 )
 
+func intArrToString(freqs []int) string {
+	var strs []string
+	for _, num := range freqs {
+		strs = append(strs, strconv.Itoa(num))
+	}
+	return strings.Join(strs, "")
+}
+
+func intArrToInt(arr []int) (int, error) { // lol
+	return strconv.Atoi(intArrToString(arr))
+}
+
 func main() {
+	freqMap := map[string]int {"467889": 0, "89": 1, "47788": 2, "77889": 3, "6789": 4, "67789": 5, "467789": 6, "889": 7, "4677889": 8, "677889": 9}
 	count := 0
 
 	file, err := os.Open("./input/8.txt")
@@ -20,14 +35,28 @@ func main() {
     scanner := bufio.NewScanner(file)
     for scanner.Scan() {
         split := strings.Split(scanner.Text(), " | ")
-		
+		clues := strings.Split(split[0], " ")
 		digits := strings.Split(split[1], " ")
-		for _, digit := range digits {
-			switch len(digit) {
-			case 2, 3, 4, 7:
-				count++
+		freqs := []int{0, 0, 0, 0, 0, 0, 0}
+		//             a  b  c  d  e  f  g
+		for _, clue := range clues {
+			for _, char := range clue {
+				freqs[char - 'a']++
 			}
 		}
+		
+		var final []int
+		for _, digit := range digits {
+			var digitFreqs []int
+			for _, char := range digit {
+				digitFreqs = append(digitFreqs, freqs[char - 'a'])
+			}
+			sort.Ints(digitFreqs)
+			mapped := freqMap[intArrToString(digitFreqs)]
+			final = append(final, mapped)
+		}
+		parsed, _ := intArrToInt(final)
+		count += parsed
     }
 
 	fmt.Println(count)
