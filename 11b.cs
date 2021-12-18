107183e2d5c36a51d766a474bf30c4b77bf72d08
Henry Langmack <Henry.langmack@gmail.com>
2021-12-18 11:55:49 -0500

    day 11...

---
+++ 11b.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Linq;
+
+namespace Aoc2021 {
+
+    public class Octo {
+        private int value;
+        public bool flashed {get; private set;} = false;
+        public int flashes {get; private set;} = 0;
+        public List<Octo> neighboring = new();
+
+        public Octo(int value) {
+            this.value = value;
+        }
+
+        public void Reset() {
+            if(this.flashed) {
+                this.value = 0;
+                this.flashed = false;
+            }
+        }
+
+        public void Update() {
+            this.value++;
+        }
+
+        public void TryFlash() {
+            if(!this.flashed && this.value > 9) {
+                this.flashed = true;
+                this.flashes++;
+                foreach(var octo in this.neighboring) octo.Update();
+                foreach(var octo in this.neighboring) octo.TryFlash();
+            }
+        }
+    }
+
+    public static class Day11A {
+        public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> source) { // Thank you stack overflow
+            return source.Select((item, index) => (item, index));
+        }
+
+        public const int STEPS = 100;
+        static Dictionary<(int x, int y), Octo> octos = new();
+
+        static void Load() {
+            foreach(var (line, y) in System.IO.File.ReadLines("./input/11.txt").WithIndex()) {
+                foreach(var (value, x) in line.ToCharArray().WithIndex()) {
+                    octos.Add((x, y), new Octo(value - '0'));
+                }
+            }
+
+            foreach(var (coord, octo) in octos) AddNeighboring(coord, octo);
+        }
+
+        static void AddNeighboring((int x, int y) coord, Octo octo) {
+            for(int i = -1; i <= 1; i++) {
+                for(int j = -1; j <= 1; j++) {
+                    if(i == 0 && j == 0) continue;
+                    var pos = (coord.x + i, coord.y + j);
+                    var neighbor = octos.ContainsKey(pos) ? octos[pos] : null;
+                    if(neighbor != null) octo.neighboring.Add(neighbor);
+                }
+            }
+        }
+
+        public static void Main(string[] args) {
+            Load();
+
+            int step = 0;
+            while(!octos.Values.All(octo => octo.flashed)) {
+                step++;
+                foreach(var octo in octos.Values) octo.Reset();
+                foreach(var octo in octos.Values) octo.Update();
+                foreach(var octo in octos.Values) octo.TryFlash();
+            }
+
+            Console.WriteLine(step);
+        }
+    }
+
+}
